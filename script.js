de5d7dea6f849c5d14c1efe614a2879c192c5524
timelessmoment05 <timelessmoment05@gmail.com>
2022-02-04 20:21:38 -0800

    JavaScript add

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 /* TO DO */
+function ratestar() {
+    var a;
+    a = document.getElementById("div1");
+    a.innerHTML = "&#xf006;";
+    setTimeout(function () {
+        a.innerHTML = "&#xf123;";
+      }, 1000);
+    setTimeout(function () {
+        a.innerHTML = "&#xf005;";
+      }, 2000);
+  }
+  ratestar();
+  setInterval(ratestar, 3000);
