afb266bb4f3ebb541ddd5876bc2c5c0c2ff7f7fa
timelessmoment05 <timelessmoment05@gmail.com>
2022-02-07 14:37:26 -0800

    Updates to portfolio HTML positioning box2

--- script.js
+++ script.js
@@ -41,54 +41,25 @@ $('html body').keydown(function(e) {
   }
 });
 
-// Swiping carousel
-document.addEventListener('touchstart', handleTouchStart, false);        
-document.addEventListener('touchmove', handleTouchMove, false);
 
-var xDown = null;                                                        
-var yDown = null;
+// Swiping???? CHECK BEFORE SUBMITTING!!!!!!!!!!!!!
+$ (".carousel").swipe ({ swipe: function (event, direction, distance, duration, fingerCount, fingerData) { if (direction == 'left') $ (this).carousel ('next'); if (direction == 'right') $ (this).carousel ('prev'); }, allowPageScroll:"vertical" });
 
-function getTouches(evt) {
-  return evt.touches ||             // browser API
-         evt.originalEvent.touches; // jQuery
-}                                                     
-                                                                         
-function handleTouchStart(evt) {
-    const firstTouch = getTouches(evt)[0];                                      
-    xDown = firstTouch.clientX;                                      
-    yDown = firstTouch.clientY;                                      
-};                                                
-                                                                         
-function handleTouchMove(evt) {
-    if ( ! xDown || ! yDown ) {
-        return;
-    }
+$(".carousel").swipe({
 
-    var xUp = evt.touches[0].clientX;                                    
-    var yUp = evt.touches[0].clientY;
-
-    var xDiff = xDown - xUp;
-    var yDiff = yDown - yUp;
-                                                                         
-    if ( Math.abs( xDiff ) > Math.abs( yDiff ) ) {/*most significant*/
-        if ( xDiff > 0 ) {
-            /* right swipe */ 
-        } else {
-            /* left swipe */
-        }                       
-    } else {
-        if ( yDiff > 0 ) {
-            /* down swipe */ 
-        } else { 
-            /* up swipe */
-        }                                                                 
-    }
-    /* reset values */
-    xDown = null;
-    yDown = null;                                             
-};
+    swipe: function(event, direction, distance, duration, fingerCount, fingerData) {
+  
+      if (direction == 'left') $(this).carousel('next');
+      if (direction == 'right') $(this).carousel('prev');
+  
+    },
+    allowPageScroll:"vertical"
+  
+  });
 
 
+
+  
 /* Animated star*/ 
 function ratestar() {
     var a;
