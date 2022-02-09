1987dceb781b432a8edc3b600d2f4304558b5ad5
timelessmoment05 <timelessmoment05@gmail.com>
2022-02-09 10:17:29 -0800

    carousel updates

--- script.js
+++ script.js
@@ -1,63 +1,15 @@
-/* Card Carousel */
-$num = $('.my-card').length;
-$even = $num / 2;
-$odd = ($num + 1) / 2;
-
-if ($num % 2 == 0) {
-  $('.my-card:nth-child(' + $even +')').addClass('active');
-  $('.my-card:nth-child(' + $even +')').prev().addClass('prev');
-  $('.my-card:nth-child(' + $even +')').next().addClass('next');
-}else {
-  $('.my-card:nth-child(' + $odd +')').addClass('active');
-  $('.my-card:nth-child(' + $odd +')').prev().addClass('prev');
-  $('.my-card:nth-child(' + $odd +')').next().addClass('next');
-}
-
-$('.my-card').click(function() {
-  $slide = $('.active').width();
-  console.log($('.active').position().left);
-
-  if ($(this).hasClass('next')) {
-    $('.card-carousel').stop(false,true).animate({left:'-=' + $slide});
-  }else if ($(this).hasClass('prev')) {
-    $('.card-carousel').stop(false,true).animate({left:'+=' + $slide});
-  }
-
-  $(this).removeClass('prev next');
-  $(this).siblings().removeClass('prev active next');
-   
-  $(this).addClass('active');
-  $(this).prev().addClass('prev');
-  $(this).next().addClass('next');
+var elem = document.querySelector('.main-carousel');
+var flkty = new Flickity( elem, {
+  // options
+  cellAlign: 'left',
+  contain: true
 });
 
-// Keyboard nav
-$('html body').keydown(function(e) {
-  if (e.keyCode == 37) {// left
-    $('.active').prev().trigger('click');
-  }
-  else if (e.keyCode == 39) {// right
-    $('.active').next().trigger('click');
-  }
+// element argument can be a selector string
+//   for an individual element
+var flkty = new Flickity( '.main-carousel', {
+  // options
 });
-
-
-// Swiping???? CHECK BEFORE SUBMITTING!!!!!!!!!!!!!
-$ (".carousel").swipe ({ swipe: function (event, direction, distance, duration, fingerCount, fingerData) { if (direction == 'left') $ (this).carousel ('next'); if (direction == 'right') $ (this).carousel ('prev'); }, allowPageScroll:"vertical" });
-
-$(".carousel").swipe({
-
-    swipe: function(event, direction, distance, duration, fingerCount, fingerData) {
-  
-      if (direction == 'left') $(this).carousel('next');
-      if (direction == 'right') $(this).carousel('prev');
-  
-    },
-    allowPageScroll:"vertical"
-  
-  });
-
-
 
   
 /* Animated star*/ 
